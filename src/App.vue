2329d1f06605ccc0dd93305ca8bff8f0112d3216
Keisuke Imura <keisuke@keisuke-imura.com>
2018-06-24 19:25:53 +0900

    基本的なtodoの動き

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="section">
+    <div class="container">
+      <h1>Todo App</h1>
+      <Todo/>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todo from './components/Todo.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Todo
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <form action="">
+      <input type="text" v-model="text">
+      <input type="submit" value="Add" v-on:click.prevent="add">
+    </form>
+    <ul>
+      <li v-for="(item, key) in list" v-if="item.status === 0">
+        {{ item.text }} <button v-on:click="done(key)">Done</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Todo',
+    data() {
+      return {
+        text: '',
+        list: [],
+      }
+    },
+    methods: {
+      add: function () {
+        this.list.push({
+          text: this.text,
+          status: 0,
+        });
+        this.text = '';
+      },
+      done: function (key) {
+        this.list[key].status = 1;
+      },
+    },
+  }
+</script>
